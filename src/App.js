614b06f34a57604673d5f8064b118de3135dfaf5
Lazar <lazarspasic96@gmail.com>
2020-01-17 16:14:30 +0100

    Create Layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layout'
 
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <p>TEST</p>
+      </Layout>
+      
     </div>
   );
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Layout = () => {
+   <>
+   <div>Toolbar, SideDrawer, Backdrop</div> 
+   <main>
+       {props.children}
+   </main>
+       
+       </>
+}
+
+export default Layout;
+
+
