ecd58cf9a3bfa1fb48fb70bea9dda5e1850e4c4b
Laks96 <lazarspasic96@gmail.com>
2020-02-01 21:41:12 +0100

    Use reduce method to check if there is no ingredients and display the message!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,19 +3,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import clasess from './Burger.module.css'
 
 const Burger = (props) => {
-    const tranformedIngredients = Object.keys(props.ingredients)
+    let tranformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
            return <BurgerIngredient key = {igKey + i} type = {igKey}/>
         });
-    });
+    }).reduce((arr, el) => {
+        console.log(el)
+        return arr.concat(el)
+    },[]);
+    console.log(tranformedIngredients);
+    if(tranformedIngredients.length === 0) {
+        tranformedIngredients = <p>Please start adding ingredients!</p>
+    } 
     return (
+        
         <div className={clasess.Burger}>
             <BurgerIngredient type={'bread-top'}/>
-   {tranformedIngredients}
+                {tranformedIngredients}
             <BurgerIngredient type={'bread-bottom'}/>
+            
      
         </div>
+        
 
     )
 }
