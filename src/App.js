d463bf88153ed7d163286373f830660ca53fc916
Laks96 <lazarspasic96@gmail.com>
2020-03-19 00:37:02 +0100

    Create Checkout Summary, and install react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/Burger-Builder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <BurgerBuilder/>
+        <Switch>
+          <Route exact path='/' component={BurgerBuilder} />
+          <Route path='/checkout' component={Checkout} />
+        </Switch>
       </Layout>
-      
+
     </div>
   );
 }
